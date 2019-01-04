ffdcc891697362141c61a72ed5bc1ced1a5b6450
Igor <igor.filipiuk@gmail.com>
2019-01-04 16:39:34 +0100

    added zippy component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { CardComponent } from './card/card.component';
 import { LikeComponent } from './like/like.component';
 import { InputFormatDirective } from './input-format.directive';
+import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { InputFormatDirective } from './input-format.directive';
     FavoriteComponent,
     CardComponent,
     LikeComponent,
-    InputFormatDirective
+    InputFormatDirective,
+    ZippyComponent
   ],
   imports: [
     BrowserModule,
