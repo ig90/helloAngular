319f5c05de4204c7ce03ee47f1ea2435c20c7bd8
Igor <igor.filipiuk@gmail.com>
2019-01-06 16:43:22 +0100

    validation by ngModel - template driven approach

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { log } from 'util';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+  log(txt) {
+    console.log(txt);
   }
+
 
 }
