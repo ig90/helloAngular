7cd3b6a94ac0b6bc87ecde05f7c700ddff890181
Igor <igor.filipiuk@gmail.com>
2019-02-20 20:10:36 +0100

    query parameters and subscribing to multiple observables

--- src/app/github-followers/github-followers.component.ts
+++ src/app/github-followers/github-followers.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubFollowersService } from '../services/github-followers.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
 
 @Component({
   selector: 'app-github-followers',
@@ -9,12 +12,37 @@ import { GithubFollowersService } from '../services/github-followers.service';
 export class GithubFollowersComponent implements OnInit {
   followers: any[];
 
-  constructor(private service: GithubFollowersService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private service: GithubFollowersService) { }
 
   ngOnInit() {
+    Observable.combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ])
+    .subscribe(combined => {
+      let id = combined[0].get('id');
+      let page = combined[1].get('page');
+
+      this.service.getAll()
+      .subscribe(
+        followers => this.followers = followers);
+    });
+    /*
+    this.route.paramMap
+    .subscribe(params => {} );
+      // const id = this.route.snapshot.paramMap.get('id');
+
+    this.route.queryParamMap
+    .subscribe(params => {} );
+      // const page = this.route.snapshot.queryParamMap.get('page');
+
     this.service.getAll()
     .subscribe(
-      followers => this.followers = followers);
+      followers => this.followers = followers
+    );
+    */
 
   }
 
