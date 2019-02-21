22c3c9e812a5340857740571d99a47b204e42c96
Igor <igor.filipiuk@gmail.com>
2019-02-21 16:57:57 +0100

    moved services to its folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { CourseComponent } from './course/course.component';
 import { CoursesComponent } from './courses.component';
 import { CoursesService } from './course/courses.service';
 import { AuthorComponent } from './author/author.component';
-import { AuthorsService } from './authors.service';
+import { AuthorsService } from './services/authors.service';
 import { SummaryPipe } from './summary.pipe';
 import { FavoriteComponent } from './favorite/favorite.component';
 import { CardComponent } from './card/card.component';
